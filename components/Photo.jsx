a5b388fca20631d5964c4799f0ab67a52d403196
Arnob Mahmud <arnob_t78@yahoo.com>
2024-09-06 20:45:57 +0200

    vercel image problem

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -5,7 +5,7 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
 
-// import image from "./assets/photo.png";
+// import image from "/assets/photo.png";
 
 const Photo = () => {
   return (
@@ -27,7 +27,7 @@ const Photo = () => {
           className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"
         >
           <Image
-            src="/assets/photo.png"
+            src="/photo.png"
             priority
             quality={100}
             fill
