bb9be90f3c471d326031bceb95774bec254e8081
Arnob Mahmud <arnob_t78@yahoo.com>
2024-09-06 19:33:15 +0200

    Vercel image loading problem

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -1,7 +1,11 @@
 "use client";
 
+
+
 import { motion } from "framer-motion";
 import Image from "next/image";
+
+// import image from "./assets/photo.png";
 
 const Photo = () => {
   return (
@@ -23,8 +27,7 @@ const Photo = () => {
           className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"
         >
           <Image
-            src={"/assets/photo.png"}
-            
+            src="/photo.png"         
             priority
             quality={100}
             fill
